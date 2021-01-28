e5946d1093efad4a52d85ddf682a133f93d626c2
Nick Pattison <pattison.nick@gmail.com>
2021-01-28 03:13:32 -0500

    Added TripleShot weapon

--- Assets/Scripts/Ships and AI/ShipSystems/_Weapon.cs
+++ Assets/Scripts/Ships and AI/ShipSystems/_Weapon.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class _Weapon : _ShipSystem {
 
     protected float delayTimer = 0;
+    public bool isAuto = false;
+    protected bool hasLetOff = false;
 
     override public void DoTick() {
         if (delayTimer > 0) delayTimer -= Time.deltaTime;
 
         if (ship.controller.wantsToAim) transform.rotation = Quaternion.LookRotation(ship.controller.dirToAim, Vector3.up);
-        if (ship.controller.wantsToAction1) DoAttack();
+        if (ship.controller.wantsToAction1) {
+            if (isAuto || hasLetOff) DoAttack();
+            hasLetOff = false;
+        } else hasLetOff = true;
     }
     virtual public void DoAttack() {
 
