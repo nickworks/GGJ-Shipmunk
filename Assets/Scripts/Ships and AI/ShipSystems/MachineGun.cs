6fd7d607b0cdff00dbeebab70b1f9dbb549b4365
Nick Pattison <pattison.nick@gmail.com>
2021-01-28 01:09:10 -0500

    Fleshed out the MachineGun logic

--- Assets/Scripts/Ships and AI/ShipSystems/MachineGun.cs
+++ Assets/Scripts/Ships and AI/ShipSystems/MachineGun.cs
@@ -5,9 +5,34 @@ using UnityEngine;
 public class MachineGun : _Weapon
 {
     public Projectile projectilePrefab;
-    
+
+    public int splitAmount = 1;
+    public float splitAngle = 0;
+
+    public float bulletsPerSecond = 10;
+
+    public float randomWidth = 0.5f;
+    public float randomAngle = 0;
+
     override public void DoAttack() {
-        Projectile p = Instantiate(projectilePrefab, transform.position, transform.rotation);
-        p.InitBullet(ship.controller.allegiance, 30);
+        if (delayTimer > 0) return;
+
+        delayTimer = 1 / bulletsPerSecond;
+
+        float yawAim = transform.eulerAngles.y;
+
+        float offsetAngle = (splitAmount / 2) * splitAngle;
+
+        for (int i = 0; i < splitAmount; i++) {
+
+            float radsAngle = Mathf.Deg2Rad * (yawAim - offsetAngle + i * splitAngle);
+            Vector3 dir = new Vector3(Mathf.Sin(radsAngle), 0, Mathf.Cos(radsAngle));
+            Vector3 off = Vector3.Cross(dir, Vector3.up);
+            off *= Random.Range(-randomWidth, randomWidth);
+
+            Projectile p = Instantiate(projectilePrefab, transform.position + off, Quaternion.LookRotation(dir, Vector3.up));
+            p.InitBullet(ship.controller.allegiance, 30);
+        }
+        
     }
 }
--- Assets/Scripts/Ships and AI/ShipSystems/_Weapon.cs
+++ Assets/Scripts/Ships and AI/ShipSystems/_Weapon.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class _Weapon : _ShipSystem {
 
+    protected float delayTimer = 0;
+
     override public void DoTick() {
+        if (delayTimer > 0) delayTimer -= Time.deltaTime;
+
         if (ship.controller.wantsToAim) transform.rotation = Quaternion.LookRotation(ship.controller.dirToAim, Vector3.up);
         if (ship.controller.wantsToAction1) DoAttack();
     }
