7b27410fef15138a56dbadf42c1b16e72ba9b7ae
Nick Pattison <pattison.nick@gmail.com>
2021-01-27 23:05:27 -0500

    Added state machine to ship

--- Assets/Scripts/Ships and AI/Spaceship.cs
+++ Assets/Scripts/Ships and AI/Spaceship.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour {
 
+    public static class States {
+        public class _State {
+            protected Spaceship ship;
+            virtual public _State Update() { return null; }
+            virtual public void OnStart(Spaceship ship) { this.ship = ship; }
+            virtual public void OnEnd() { }
+        }
+        public class Moving : _State {
+            public override _State Update() {
+
+                ship.DoDrive();
+                ship.DoAim();
+                ship.DoPhysTick();
+
+                return null;
+            }
+        }
+        public class Dashing : _State {
+
+        }
+    }
+
     public float speed = 5;
     private Vector3 velocity;
 
     public Controller controller { get; private set; }
-
+    public States._State state { get; private set; }
     public Transform weaponArt;
+
 
     void Start() {
         controller = GetComponent<Controller>();
@@ -17,16 +40,28 @@ public class Spaceship : MonoBehaviour {
     
     void Update() {
 
-        if (controller.wantsToMove) AddForce(controller.dirToMove * speed * Time.deltaTime);
-        if (controller.wantsToAim) weaponArt.rotation = Quaternion.LookRotation(controller.dirToAim, Vector3.up);
-        EulerPhysTick();
+        if (state == null) ChangeState(new States.Moving());
+        ChangeState(state.Update());
+
+    }
+    private void ChangeState(States._State next) {
+        if (next == null) return;
+        if (state != null) state.OnEnd();
+        state = next;
+        state.OnStart(this);
     }
 
     public void AddForce(Vector3 force) {
         force.y = 0;
         velocity += force;
     }
-    public void EulerPhysTick() {
+    void DoDrive() {
+        if (controller.wantsToMove) AddForce(controller.dirToMove * speed * Time.deltaTime);
+    }
+    void DoAim() {
+        if (controller.wantsToAim) weaponArt.rotation = Quaternion.LookRotation(controller.dirToAim, Vector3.up);
+    }
+    void DoPhysTick() {
         transform.position += velocity * Time.deltaTime;
     }
 
